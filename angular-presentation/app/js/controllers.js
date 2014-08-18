ad0752487799d2e46420f06e71ca2c428974516c
kiwi <jcwen@thoughtworks.com>
2014-08-18 15:31:19 +0800

    step 5: dependency injection

--- angular-presentation/app/js/controllers.js
+++ angular-presentation/app/js/controllers.js
@@ -1,7 +1,7 @@
 var phoneApp = angular.module('phoneApp', []);
 
-phoneApp.controller('PhoneListCtrl', function ($scope, $http) {
+phoneApp.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
 	$http.get('phones/phones.json').success(function (data) {
 		$scope.phones = data;
 	});
-});
+}]);
