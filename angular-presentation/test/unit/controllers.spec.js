54b5afe32d3aacb4077e1ff539971554bda7dbff
kiwi <jcwen@thoughtworks.com>
2014-08-18 15:29:56 +0800

    step 5: dependency injection

--- angular-presentation/test/unit/controllers.spec.js
+++ angular-presentation/test/unit/controllers.spec.js
@@ -1,14 +1,36 @@
 'use strict';
 
 describe('Simple controllers', function() {
+	var scope, ctrl, $httpBackend;
+
+	var phonesRespond = function () {
+		[
+			{"name": "Nexus S",
+			 "snippet": "Fast just got faster with Nexus S."},
+			{"name": "Motorola XOOM™ with Wi-Fi",
+			 "snippet": "The Next, Next Generation tablet."}
+		]		
+	}
+
 	beforeEach(module('phoneApp'));
 
-	describe('phone controllers', function() {
-		it('should get 2 phones', inject(function($controller) {
-	    	var scope = {},
-	        	ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
-	      	expect(scope.phones.length).toBe(2);
-    	}))
-	})
+	beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+		$httpBackend = _$httpBackend_;
+		$httpBackend.expectGET("phones/phones.json")
+			.respond(phonesRespond());
+
+		scope = $rootScope.$new();
+		ctrl = $controller('PhoneListCtrl', {$scope: scope});
+	}));
+
+
+	it ('should create "phones" model with 2 phones fetched from xhr', function () {
+		expect(scope.phones).toBeUndefined();
+		$httpBackend.flush();
+
+
+		expect(scope.phones).toEqual(phonesRespond());
+	});
+
 });
